a4ae78ddb83c2a2eb8e0d20fbaa48dab067b4e03
kkassin <kkassin@edr43705.dip.tu-dresden.de>
2018-09-20 13:54:16 +0200

    update docker files and starter scripts accoridng to the changes of the commit before

--- Docker1/start_docker.sh
+++ Docker1/start_docker.sh
@@ -1,5 +1,4 @@
 eval $(minikube docker-env)
-minikube ip > cluster_ip.txt
 docker build -t spring1:v1 .
 kubectl create -f ./deployment.yml
 kubectl create -f ./service.yml
--- Docker4/start_docker.sh
+++ Docker4/start_docker.sh
@@ -1,5 +1,4 @@
 eval $(minikube docker-env)
-minikube ip > cluster_ip.txt
 docker build -t spring4:v1 .
 kubectl create -f ./deployment.yml
 kubectl create -f ./service.yml
