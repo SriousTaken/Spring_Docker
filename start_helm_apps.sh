71a986c25844ae8c805117b925575496bb7d6b7f
kkassin <kkassin@edr21356.dip.tu-dresden.de>
2018-10-01 15:48:21 +0200

    implemented Prometheus und Grafana Support

--- start_helm_apps.sh
+++ start_helm_apps.sh
@@ -1,6 +1,16 @@
+#MongoDB
 helm install --name mongodb \
-  --set mongodbRootPassword=pw,mongodbUsername=user,mongodbPassword=pw,mongodbDatabase=my-database \
-  stable/mongodb
+  			 --set mongodbRootPassword=pw,mongodbUsername=user,mongodbPassword=pw,mongodbDatabase=my-database \
+  			 stable/mongodb
+#Prometheus
+#helm install --name prometheus \
+#			 --set server.service.type=NodePort,serverFiles.prometheus.yml=prometheus_scrape.yml \
+#			 stable/prometheus
+helm install stable/prometheus --name prometheus -f prometheus.yaml
+helm install --name grafana \
+			 --set service.type=NodePort \
+	 		 stable/grafana  
+
 #helm install --name rabbitmq \
 #  --set rabbitmq.username=admin,rabbitmq.password=pw,rabbitmq.erlangCookie=secretcookie \
 #    stable/rabbitmq
